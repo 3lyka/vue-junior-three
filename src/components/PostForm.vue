1f5cd778e997126a0132d63aea17dd3d08587ae6
vadim.garaev <68156261+3lyka@users.noreply.github.com>
2022-12-03 17:01:59 +0300

    init vue

---
+++ src/components/PostForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <h2>Создание поста</h2>
+      <form method="post">
+        <input @input="inputTitle" v-bind:value="title" class="title__post" type="text" name="" id="" placeholder="Название поста">
+        <input @input="inputBody" v-bind:value="body" class="body__post" type="text" name="" id="" placeholder="Описание поста">
+        <button class="btn button__add-post" type="submit" @click.prevent="createPost">Добавить</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostForm"
+}
+</script>
+
+<style scoped>
+
+</style>
